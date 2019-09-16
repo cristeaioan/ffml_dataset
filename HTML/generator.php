c46a5ceea0b354f5f2de9537264ffbf74eae1b42
cristeaioan <c.ioan12@yahoo.com>
2019-09-16 14:41:59 +0300

    Added new types of foods (Bubble Mint Gum; Cheese; Choco Balls Cereals; Danish Cookies; Danish Cookies With Sugar; Eukaliptus Menthol Bonbons; Ferrero-Yougurette Chocolate bar Filled With Strawberry Yogurt; Fresh Garden Salad; Homemade Chili Jam; Mentos Mint; Milka Alpin Milk Chocolate Bar; Milka Bubbly White Aerated Chocolate Bar; Mixed Fruits Bubble Gum; Pork Meat Pate; Raffaelo balls; Simple Croutons; Spicy Pineapple Sauce; Sugus Milk Toffee; Wholegrain Baguette With Flax Seeds; Zacusca With Eggplant).

--- HTML/generator.php
+++ HTML/generator.php
@@ -6,32 +6,49 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <style>
+        .items {
+            max-width: 600px;
+            margin: 0 auto;
+        }
+
+        a {
+            font-family: 'Arial';
+            font-size: 19px;
+            text-decoration: none;
+            color: #444444;
+        }
+    </style>
 </head>
 <body>
 
-<?php
+<div class="items">
+    <?php
 
-//$dir = scandir('../FOOD');
-//var_dump($dir);
-//
-//foreach ( $dir as $item) {
-//    var_dump(scandir($item));
-//}
+    //$dir = scandir('../FOOD');
+    //var_dump($dir);
+    //
+    //foreach ( $dir as $item) {
+    //    var_dump(scandir($item));
+    //}
 
-$iterator = new RecursiveIteratorIterator(
-    new RecursiveDirectoryIterator('../FOOD',
-        FilesystemIterator::SKIP_DOTS),
-    RecursiveIteratorIterator::SELF_FIRST);
+    $iterator = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator('../FOOD',
+            FilesystemIterator::SKIP_DOTS),
+        RecursiveIteratorIterator::SELF_FIRST);
 
-foreach($iterator as $file) {
+    foreach($iterator as $file) {
 
-    if($file->isDir()) {
-        $indent = str_repeat('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;', $iterator->getDepth());
-        echo $indent . '<a href="' . $file . '">' . ucwords(strtolower($file->getFilename())) . '</a><br>';
+        if($file->isDir()) {
+            $indent = str_repeat('&nbsp;', $iterator->getDepth() * 10);
+            echo $indent . '<a href="' . $file . '">' . ucwords(strtolower($file->getFilename())) . '</a><br><br>';
+        }
     }
-}
 
-?>
+    ?>
+</div>
+
 
 </body>
 </html>
